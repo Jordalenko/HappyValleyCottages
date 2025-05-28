3c012ece30322d1558ce3ab0964a50648cad2569
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-05-28 11:36:01 +0100

    add model registration to hvc app admin.py and db csrf token code

--- happyvalleycottages/admin.py
+++ happyvalleycottages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Guest, Cottage, Reservation, Review
 
 # Register your models here.
+
+admin.site.register(Guest)
+admin.site.register(Cottage)
+admin.site.register(Reservation)
+admin.site.register(Review)
