9958778a9018214cc88b551341271a388990cf6a
JordanAcomba <42304760+Jordalenko@users.noreply.github.com>
2025-05-29 15:14:33 +0100

    add minor changes to url and view files for Cottages model interaction with template

--- happyvalleycottages/views.py
+++ happyvalleycottages/views.py
@@ -6,5 +6,10 @@ from .models import Cottage
 
 
 class CottageList(generic.ListView):
+    # Retrieves Cottage objects from database.
     queryset = Cottage.objects.all()
+    # Number of cottages to display per page.
+    paginate_by = 2  
     template_name = "cottage_list.html"
+    # Allows for loops in template.
+    context_object_name = 'cottages'
